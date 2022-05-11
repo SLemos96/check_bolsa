b095c3a978ac4c6478fcd8db0e18dc5a9b2ce3b7
Samuel Lemos <samuel.afonso2008@hotmail.com>
2022-05-10 22:49:55 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/check_bolsa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.check_bolsa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+// import 'result.dart';
+
+const request =
+    "https://api.hgbrasil.com/finance/stock_price?key=410b24bd&symbol=";
+
+void main() {
+  runApp(const MyApp());
+}
+
+Future<Map> getData(acao) async {
+  http.Response response = await http.get(request + acao);
+  return json.decode(response.body);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bolsa',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const MyHomePage(title: 'Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _valorAcao = 40.2;
+  // String _acao = ""; //chave de busca
+  TextEditingController acaoController = TextEditingController();
+  TextEditingController pesoController = TextEditingController();
+
+  void _buscar() {
+    //requisição tem que ser feita em cima dessa string
+    print(request + acaoController.text);
+    getData(acaoController.text);
+
+    // debugPrint("Peso ${request} e altura ${acaoController.text}");
+    //debugPrint("$imc");
+
+    // Navigator.push(context, MaterialPageRoute(builder: (context) => Result(_imagem, _texto)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_valorAcao',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            TextFormField(
+              // keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  labelText: "Código da Ação",
+                  labelStyle: TextStyle(color: Colors.blueAccent)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+              controller: acaoController,
+              validator: (String? value) {
+                if (value != null && value.isEmpty) return "Insira seu peso!";
+              },
+            ),
+            Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                    height: 50.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // if (_formKey.currentState.validate()) {
+                        _buscar();
+                        // }
+                      },
+                      child: Text(
+                        "Buscar",
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.blue,
+                          textStyle: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold)),
+                    ))),
+          ],
+        ),
+      ),
+    );
+  }
+}
